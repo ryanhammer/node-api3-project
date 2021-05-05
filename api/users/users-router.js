e15ad9ff8ecbeec8721441dbfe4c2d8269637e42
rhammer35 <rhammer35@icloud.com>
2021-05-05 16:42:40 -0400

    added get, post, and put users endpoints

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,28 +1,46 @@
 const express = require('express');
 
 // You will need `users-model.js` and `posts-model.js` both
+const Posts = require('../users/users-model');
+const Users = require('../users/users-model');
+
 // The middleware functions also need to be required
+const { validateUserId, validateUser, validatePost } = require('../middleware/middleware');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // RETURN AN ARRAY WITH ALL THE USERS
+  Users.find(req.query)
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: "The users information could not be retrieved",
+      });
+    });
 });
 
-router.get('/:id', (req, res) => {
-  // RETURN THE USER OBJECT
-  // this needs a middleware to verify user id
+router.get('/:id', validateUserId, (req, res) => {
+  res.status(200).json(req.user);
 });
 
-router.post('/', (req, res) => {
-  // RETURN THE NEWLY CREATED USER OBJECT
-  // this needs a middleware to check that the request body is valid
+router.post('/', validateUser, (req, res) => {
+  Users.add(req.body)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: error.message,
+        stack: error.stack
+      });
+    })
 });
 
-router.put('/:id', (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
+router.put('/:id', validateUser, validateUserId, (req, res) => {
+  res.status(201).json(req.user);
 });
 
 router.delete('/:id', (req, res) => {
@@ -42,3 +60,4 @@ router.post('/:id/posts', (req, res) => {
 });
 
 // do not forget to export the router
+module.exports = router;
