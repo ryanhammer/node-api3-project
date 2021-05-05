d49071c442a3cca19e01d6e0b5c0afd89d64911f
rhammer35 <rhammer35@icloud.com>
2021-05-05 16:22:11 -0400

    connected middleware functions to users-router and global middleware and users-router to server.js

--- api/server.js
+++ api/server.js
@@ -6,6 +6,12 @@ const server = express();
 server.use(express.json());
 
 // global middlewares and the user's router need to be connected here
+const { logger } = require('./middleware/middleware');
+const usersRouter = require('./users/users-router');
+
+server.use(logger);
+
+server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
