2f29441ffe427f90e0ef009d1cb9e97ba3ac9721
rhammer35 <rhammer35@icloud.com>
2021-05-05 15:48:51 -0400

    created validateUserID middleware function in middleware.js file and exported it to other modules

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,3 +1,6 @@
+const users = require('../users/users-model');
+const Users = require('../users/users-model');
+
 function logger(req, res, next) {
   console.log(`
     ${req.method} request to ${req.baseUrl}${req.url} endpoint
@@ -5,17 +8,32 @@ function logger(req, res, next) {
   next();
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+const validateUserId = async (req, res, next) => {
+  try {
+    const user = await Users.findById(req.params.id);
+    if (!user) {
+      next({
+        status: 404,
+        message: "user not found"
+      });
+    } else {
+      req.user = user;
+      next();
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: err.message
+    });
+  }
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
 }
 
-function validatePost(req, res, next) {
+function validateuser(req, res, next) {
   // DO YOUR MAGIC
 }
 
 // do not forget to expose these functions to other modules
-module.exports = { logger };
+module.exports = { logger, validateUserId };
